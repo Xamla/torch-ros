f951c1ad0a28d4da137399a954daa04861d66721
MichaelGhaben <michaelghaben@gmail.com>
2016-04-04 17:15:41 -0400

    Fixed getCount

--- test/test_subscription.lua
+++ test/test_subscription.lua
@@ -15,7 +15,7 @@ nodehandle:subscribe("chatter", string_spec, 100, msgbuf)
 
 while ros.ok() do
   sys.sleep(0.1)
-  while msgbuf:count() > 0 do
+  while msgbuf:getCount() > 0 do
     local msg_bytes = msgbuf:read()
     local msg = ros.Message(string_spec, true)
     msg:deserialize(msg_bytes)
