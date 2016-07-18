93c0f4e2989ad57540ea5484e1c77a972a67d55e
Andreas Köpf <andreas.koepf@xamla.com>
2016-07-18 18:18:47 +0200

    Fix missing closing parenthesis in subscribe.lua

--- demo/subscribe.lua
+++ demo/subscribe.lua
@@ -1,4 +1,12 @@
+--[[
+This demo shows how to subscribe to a message topic and receive
+incoming messages (in this case simple std_msg/String messages).
+You can run `publish.lua` as a message generating counter part
+to send string messages that are displayed by this demo.
+]]
+
 ros = require 'ros'
+
 
 ros.init('subscribe_demo')
 
@@ -9,7 +17,7 @@ nodehandle = ros.NodeHandle()
 
 -- subscribe to dummy_chat topic with 100 messages back-log
 -- transport_options (arguments 4 & 5) are optional - used here only for demonstration purposes
-subscriber = nodehandle:subscribe("dummy_chat", 'std_msgs/String', 100, { 'udp', 'tcp' }, { tcp_nodelay = true }
+subscriber = nodehandle:subscribe("dummy_chat", 'std_msgs/String', 100, { 'udp', 'tcp' }, { tcp_nodelay = true })
 
 -- register a callback function that will be triggered from ros.spinOnce() when a message is available.
 subscriber:registerCallback(function(msg, header)
